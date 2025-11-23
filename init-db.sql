0fc9aaec8cbd7eb8465c63000378f321f026fa4b
Nathan Trost <n.d.trost@gmail.com>
2025-11-23 16:20:22 -0600

    Update db script

--- init-db.sql
+++ init-db.sql
@@ -13,7 +13,7 @@ ALTER TABLE accounts ADD CONSTRAINT verify_type
 CHECK (type IN ('checking', 'savings', 'credit'));
 
 -- LOAD DATA
-INSERT INTO accounts 
+INSERT INTO accounts
     (account_number, name, amount, type)
 VALUES
     (1, 'Johns Checking', 1000, 'checking'),
@@ -21,17 +21,17 @@ VALUES
     (4, 'Bobs Checking', 40000, 'checking'),
     (5, 'Bills Savings', 50000, 'savings'),
     (7, 'Nancy Checking', 70000, 'checking'),
-    (8, 'Nancy Savings', 80000, 'savings');
-    (10, "Ron's Checking", 300, 'checking');
-    (11, "Barry's Checking", 75.48, 'checking')
+    (8, 'Nancy Savings', 80000, 'savings'),
+    (10, 'Ron Checking', 300, 'checking'),
+    (11, 'Barry Checking', 75.48, 'checking');
 
 INSERT INTO accounts
     (account_number, name, amount, type, credit_limit)
 VALUES
     (3, 'Jills Credit', -3000, 'credit', 10000),
     (6, 'Bills Credit', -60000, 'credit', 60000),
-    (9, 'Nancy Credit', -90000, 'credit', 100000);
-    (12, "Diane's Credit", -500, 'credit', 500)
+    (9, 'Nancy Credit', -90000, 'credit', 100000),
+    (12, 'Diane Credit', -500, 'credit', 500);
 
 
 -- Create transactions table so we can reference it for our transaction and amount calculations rather than rely on caching or memory
